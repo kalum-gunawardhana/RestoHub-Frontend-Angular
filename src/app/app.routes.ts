1af754df6861262f1c2b29c6eb03a1cd9526aa04
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-13 18:00:54 +0530

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './auth-components/signup/signup.component';
+import { LoginComponent } from './auth-components/login/login.component';
 
 export const routes: Routes = [
-    { path: "signup", component: SignupComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route
+    { path: '**', redirectTo: '/login' } // Fallback route
 ];
